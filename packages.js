9b8e3f6b1ccf59053b51667498d31b745ddcff69
okfornow <111639017+okfornow@users.noreply.github.com>
2022-08-24 16:14:17 -0400

    Add files via upload

---
+++ packages.js
@@ -0,0 +1,237 @@
+{
+    "skip": 0,
+    "limit": 20,
+    "data": [{
+        "totalMinted": 0,
+        "status": 0,
+        "subItems": ["F1", "DB", "Pistol", {
+            "sku": "Perk",
+            "variants": [{
+                "level": 1,
+                "percentage": 100
+            }]
+        }, {
+            "sku": "Perk",
+            "variants": [{
+                "level": 2,
+                "percentage": 100
+            }]
+        }, {
+            "sku": "Perk",
+            "variants": [{
+                "level": 3,
+                "percentage": 100
+            }]
+        }, {
+            "sku": "Perk",
+            "variants": [{
+                "level": 4,
+                "percentage": 100
+            }]
+        }, "Knife", "Grenade"],
+        "items": [{
+            "sku": "F1",
+            "name": "F1",
+            "description": "F1",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.548Z",
+            "updatedAt": "2022-03-22T10:41:59.548Z",
+            "id": "6239a7f77e3770332aa92b58"
+        }, {
+            "sku": "DB",
+            "name": "Double barrel shotgun",
+            "description": "DB",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.553Z",
+            "updatedAt": "2022-03-22T10:41:59.553Z",
+            "id": "6239a7f77e3770332aa92b5a"
+        }, {
+            "sku": "Pistol",
+            "name": "M1911",
+            "description": "Pistol",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.563Z",
+            "updatedAt": "2022-03-22T10:41:59.563Z",
+            "id": "6239a7f77e3770332aa92b5e"
+        }, {
+            "sku": "Perk",
+            "name": "Perk",
+            "description": "Perk",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.572Z",
+            "updatedAt": "2022-03-22T10:41:59.572Z",
+            "id": "6239a7f77e3770332aa92b62"
+        }, {
+            "sku": "Perk",
+            "name": "Perk",
+            "description": "Perk",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.572Z",
+            "updatedAt": "2022-03-22T10:41:59.572Z",
+            "id": "6239a7f77e3770332aa92b62"
+        }, {
+            "sku": "Perk",
+            "name": "Perk",
+            "description": "Perk",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.572Z",
+            "updatedAt": "2022-03-22T10:41:59.572Z",
+            "id": "6239a7f77e3770332aa92b62"
+        }, {
+            "sku": "Perk",
+            "name": "Perk",
+            "description": "Perk",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.572Z",
+            "updatedAt": "2022-03-22T10:41:59.572Z",
+            "id": "6239a7f77e3770332aa92b62"
+        }, {
+            "sku": "Knife",
+            "name": "Combat Knife",
+            "description": "Combat Knife",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.560Z",
+            "updatedAt": "2022-03-22T10:41:59.560Z",
+            "id": "6239a7f77e3770332aa92b5c"
+        }, {
+            "sku": "Grenade",
+            "name": "Grenade",
+            "description": "Grenade",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.565Z",
+            "updatedAt": "2022-03-22T10:41:59.565Z",
+            "id": "6239a7f77e3770332aa92b60"
+        }],
+        "symbol": "ETH",
+        "price": 1,
+        "name": "ZOMBIE KILLER",
+        "description": "Knife + F1 + Pistol + Shotgun + 4 perks + Grenade",
+        "image": "",
+        "type": 3,
+        "createdAt": "2022-03-22T10:41:59.485Z",
+        "updatedAt": "2022-03-22T10:41:59.485Z",
+        "id": "6239a7f77e3770332aa92b1c"
+    }, {
+        "totalMinted": 0,
+        "status": 0,
+        "subItems": ["AKM", "DB", "Pistol", "Perk", "Knife"],
+        "items": [{
+            "sku": "AKM",
+            "name": "AKM",
+            "description": "AKM",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.540Z",
+            "updatedAt": "2022-03-22T10:41:59.540Z",
+            "id": "6239a7f77e3770332aa92b54"
+        }, {
+            "sku": "DB",
+            "name": "Double barrel shotgun",
+            "description": "DB",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.553Z",
+            "updatedAt": "2022-03-22T10:41:59.553Z",
+            "id": "6239a7f77e3770332aa92b5a"
+        }, {
+            "sku": "Pistol",
+            "name": "M1911",
+            "description": "Pistol",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.563Z",
+            "updatedAt": "2022-03-22T10:41:59.563Z",
+            "id": "6239a7f77e3770332aa92b5e"
+        }, {
+            "sku": "Perk",
+            "name": "Perk",
+            "description": "Perk",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.572Z",
+            "updatedAt": "2022-03-22T10:41:59.572Z",
+            "id": "6239a7f77e3770332aa92b62"
+        }, {
+            "sku": "Knife",
+            "name": "Combat Knife",
+            "description": "Combat Knife",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.560Z",
+            "updatedAt": "2022-03-22T10:41:59.560Z",
+            "id": "6239a7f77e3770332aa92b5c"
+        }],
+        "symbol": "ETH",
+        "price": 0.5,
+        "name": "ASSASSIN",
+        "description": "Knife + AK47 + Pistol + Shotgun + 1 random perk",
+        "image": "",
+        "type": 2,
+        "createdAt": "2022-03-22T10:41:59.485Z",
+        "updatedAt": "2022-03-22T10:41:59.485Z",
+        "id": "6239a7f77e3770332aa92b1b"
+    }, {
+        "totalMinted": 0,
+        "status": 0,
+        "subItems": ["MP5", "Pistol", "Knife"],
+        "items": [{
+            "sku": "MP5",
+            "name": "MP5",
+            "description": "MP5",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.544Z",
+            "updatedAt": "2022-03-22T10:41:59.544Z",
+            "id": "6239a7f77e3770332aa92b56"
+        }, {
+            "sku": "Pistol",
+            "name": "M1911",
+            "description": "Pistol",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.563Z",
+            "updatedAt": "2022-03-22T10:41:59.563Z",
+            "id": "6239a7f77e3770332aa92b5e"
+        }, {
+            "sku": "Knife",
+            "name": "Combat Knife",
+            "description": "Combat Knife",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.560Z",
+            "updatedAt": "2022-03-22T10:41:59.560Z",
+            "id": "6239a7f77e3770332aa92b5c"
+        }],
+        "symbol": "ETH",
+        "price": 0.3,
+        "name": "SURVIVOR",
+        "description": "Knife + MP5 + Pistol",
+        "image": "",
+        "type": 1,
+        "createdAt": "2022-03-22T10:41:59.485Z",
+        "updatedAt": "2022-03-22T10:41:59.485Z",
+        "id": "6239a7f77e3770332aa92b1a"
+    }, {
+        "totalMinted": 0,
+        "status": 0,
+        "subItems": ["Knife", "Pistol"],
+        "items": [{
+            "sku": "Knife",
+            "name": "Combat Knife",
+            "description": "Combat Knife",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.560Z",
+            "updatedAt": "2022-03-22T10:41:59.560Z",
+            "id": "6239a7f77e3770332aa92b5c"
+        }, {
+            "sku": "Pistol",
+            "name": "M1911",
+            "description": "Pistol",
+            "image": "",
+            "createdAt": "2022-03-22T10:41:59.563Z",
+            "updatedAt": "2022-03-22T10:41:59.563Z",
+            "id": "6239a7f77e3770332aa92b5e"
+        }],
+        "symbol": "ETH",
+        "price": 0.1,
+        "name": "AMATEUR",
+        "description": "Knife + Pistol",
+        "image": "",
+        "type": 0,
+        "createdAt": "2022-03-22T10:41:59.485Z",
+        "updatedAt": "2022-03-22T10:41:59.485Z",
+        "id": "6239a7f77e3770332aa92b19"
+    }]
+}
